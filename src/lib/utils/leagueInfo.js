1cef078060f32f44ddd5222465b54d8a799fb8b4
thinh-n <104735896+thinh-n@users.noreply.github.com>
2025-09-26 13:49:16 -0700

    Update leagueInfo.js
    changed nikhil's favorite player to null

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -134,7 +134,7 @@ export const managers = [
         link: 6, // manager array number within this array, or null to link back to all managers page
         image: "/managers/everyone.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
-      "favoritePlayer": "Ronde Barber", // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
+      "favoritePlayer": null, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
       "valuePosition": "RB", // (optional) Favorite position (QB, WR, RB, TE, etc.)
       "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
       "philosophy": "Figure out what you do",
