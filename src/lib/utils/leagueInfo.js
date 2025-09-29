f00c6520a2723b631715a4fd014dc28060f2dc10
thinh-n <104735896+thinh-n@users.noreply.github.com>
2025-09-28 22:55:19 -0700

    Update leagueInfo.js
    enabled blog

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -3,7 +3,7 @@ export const leagueID = "1260303696181338112"; // your league ID
 export const leagueName = "League of Our Own"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
-export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
+export const enableBlog = true; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
 /*   STEP 2   */
 export const homepageText = `
