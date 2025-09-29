16aabf522d07289dbd5ddb559b218543eda807cc
thinh-n <104735896+thinh-n@users.noreply.github.com>
2025-09-28 23:01:46 -0700

    Update leagueInfo.js
    updated nikhil's picture pathway

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -125,7 +125,7 @@ export const managers = [
       "tookOver": 2021, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Austin, TX", // (optional)
       "bio": "University of Michigan alum, avid mens league soccer player, music enthusiast, reluctant consultany",
-      "photo": "/managers/nikhil.jpg", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/send_pic.png", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2021, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "tb", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
