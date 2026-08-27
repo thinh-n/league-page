a249eaf971f0738180647114fb2c8b9e4f06265f
thinh-n <104735896+thinh-n@users.noreply.github.com>
2026-08-26 22:11:11 -0700

    Update +page.svelte

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -147,7 +147,7 @@
     
     <h3 class="subSectionHeading" bind:this={oneOne}>1.1 Positional Breakdown</h3>
     
-    <p>28 Total Players</p>
+    <p>29 Total Players</p>
     
     <p class="underscore">Starters</p>
     <ul>
